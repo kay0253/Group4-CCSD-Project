bc1c2019a409917fe0eba23db61b6fed3809c23b
SalamMuhsein <salammuhsein13@gmail.com>
2024-11-20 17:13:59 +0800

    WebsiteTexts tests

--- group4-main/ccsd/src/main/java/com/example/ccsd/WebsiteTexts/WebsiteTexts.java
+++ group4-main/ccsd/src/main/java/com/example/ccsd/WebsiteTexts/WebsiteTexts.java
@@ -1,5 +1,11 @@
 package com.example.ccsd.WebsiteTexts;
 
 public class WebsiteTexts {
-    
+    public static void main(String[] args){
+
+    String PageTitle = "Empowering Business with cutting edge IT solutions";
+    String headerText = ""
+    String bodyText = 
+    String footerText = 
+}
 }
